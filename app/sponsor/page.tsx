53cb5dc18550fef0136736845dd9dafbc74383e7
toby-bridges <59594712+toby-bridges@users.noreply.github.com>
2025-12-15 19:10:41 +0800

    Update page.tsx

--- app/sponsor/page.tsx
+++ app/sponsor/page.tsx
@@ -1,5 +1,2 @@
 import { redirect } from "next/navigation";
-
-export default function SponsorPage() {
-  redirect("/dashboard");
-}
+export default function SponsorPage(){ redirect("/dashboard"); }
